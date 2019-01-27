ec2228078018f5f179edc14fa67676d0cdb5f75f
Georgio Klironomos <gklirono@ucsc.edu>
2019-01-26 22:48:01 -0800

    Made Casey Bouncy!
    -kinda rough, will clean up

---
+++ Assets/Bouncy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bouncy : MonoBehaviour
+{
+    
+
+    // public int speed;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //Debug.Log("Enter");
+            other.GetComponent<Player>().jumpHeight *= 2;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //Debug.Log("Enter");
+            other.GetComponent<Player>().jumpHeight /= 2;
+        }
+    }
+
+}
