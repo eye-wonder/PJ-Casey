f61158ca04bc1f1bcc73ec2a9abe642f5cbb2b32
Brady Moore <brdy96@gmail.com>
2019-01-26 22:08:41 -0800

    Added Animation Logic

--- Assets/Scripts/ThrowPillow.cs
+++ Assets/Scripts/ThrowPillow.cs
@@ -33,6 +33,7 @@ public class ThrowPillow : MonoBehaviour
 
     private Transform pillow;
 
+    private Animator animator;
 
     private void Awake()
     {
@@ -44,6 +45,7 @@ public class ThrowPillow : MonoBehaviour
         if (!arcRenderer) { this.GetComponentInChildren<LaunchArcRenderer>(); }
         _facingRight = this.GetComponent<Player>().movingRight;
         arcRenderer.RenderArc(new Vector2(0f, 0f), 10, 0, 0);
+        animator = this.GetComponent<Animator>();
     }
 
     private void Update()
@@ -59,7 +61,10 @@ public class ThrowPillow : MonoBehaviour
         //Render Arc preview
         if (Input.GetMouseButton(0))
         {
+            animator.SetBool("iswalking", false);
+            animator.SetBool("isThrowing", true);
             this.GetComponent<Player>().canMove = false;
+
 
             //make the line green if you can throw, red otherwise
             if (_pillowExists)
@@ -95,6 +100,8 @@ public class ThrowPillow : MonoBehaviour
         //Throw pillow
         if (Input.GetMouseButtonUp(0))
         {
+            animator.SetBool("isThrowing", false);
+            animator.SetBool("hasPillow", false);
             this.GetComponent<Player>().canMove = true;
 
             if ((_facingRight && _forceApplied.x >= 0) && !_pillowExists) // Facing Right
@@ -130,6 +137,7 @@ public class ThrowPillow : MonoBehaviour
         {
             _pillowComingBack = true;
             pillow.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            animator.SetBool("isThrowing", false);
         }
 
         if (_pillowComingBack)
@@ -140,6 +148,8 @@ public class ThrowPillow : MonoBehaviour
                 Destroy(pillow.gameObject);
                 _pillowExists = false;
                 _pillowComingBack = false;
+                animator.SetBool("hasPillow", true);
+
             }
             else //move it towards you
             {
